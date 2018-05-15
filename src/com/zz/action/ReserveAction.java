071fc943bde3e6a7076e2e785d48967061982f61
DainelLiu <523241535@qq.com>
2018-05-15 14:03:42 +0800

    统计

--- src/com/zz/action/ReserveAction.java
+++ src/com/zz/action/ReserveAction.java
@@ -16,38 +16,42 @@ import com.zz.dao.IBoardRoomDao;
 import com.zz.dao.IDepartmentDao;
 import com.zz.dao.IReserveDao;
 import com.zz.dao.IUsersDao;
+import com.zz.model.Department;
 import com.zz.model.Reserve;
 import com.zz.util.JsonUtil;
 import com.zz.util.PageBean;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 @Scope("prototype")
 @ParentPackage("struts-default")
-//表示继承的父包
+// 表示继承的父包
 @Namespace(value = "/reserve")
 public class ReserveAction {
-	
+
 	private IReserveDao reserveDao;
-	
+
 	public IReserveDao getReserveDao() {
 		return reserveDao;
 	}
-	@Resource(name="ReserveDao")
+
+	@Resource(name = "ReserveDao")
 	public void setReserveDao(IReserveDao reserveDao) {
 		this.reserveDao = reserveDao;
 	}
-	
-private IBoardRoomDao boardroomDao;
-	
+
+	private IBoardRoomDao boardroomDao;
+
 	public IBoardRoomDao getBoardRoomDao() {
 		return boardroomDao;
 	}
-	@Resource(name="BoardRoomDao")
+
+	@Resource(name = "BoardRoomDao")
 	public void setBoardRoomDao(IBoardRoomDao boardroomDao) {
 		this.boardroomDao = boardroomDao;
 	}
-	
+
 	private IDepartmentDao departmentDao;
 
 	public IDepartmentDao getDepartmentDao() {
@@ -58,7 +62,7 @@ private IBoardRoomDao boardroomDao;
 	public void setDepartmentDao(IDepartmentDao departmentDao) {
 		this.departmentDao = departmentDao;
 	}
-	
+
 	private IUsersDao usersDao;
 
 	public IUsersDao getUsersDao() {
@@ -69,23 +73,22 @@ private IBoardRoomDao boardroomDao;
 	public void setUsersDao(IUsersDao usersDao) {
 		this.usersDao = usersDao;
 	}
-	
 
 	/**
 	 * 保存预约信息
+	 * 
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	@Action(value="save")
-	public String save() throws IOException{
-		
+	@Action(value = "save")
+	public String save() throws IOException {
+
 		String resBId = ServletActionContext.getRequest().getParameter("resBId");
 		String resDId = ServletActionContext.getRequest().getParameter("resDId");
 		String resUId = ServletActionContext.getRequest().getParameter("resUId");
 		String resStarttime = ServletActionContext.getRequest().getParameter("resStarttime");
 		String resEndtime = ServletActionContext.getRequest().getParameter("resEndtime");
-		
-		
+
 		Reserve reserve = new Reserve();
 		reserve.setResBId(boardroomDao.getById(resBId));
 		reserve.setResDId(departmentDao.getById(resDId));
@@ -94,35 +97,37 @@ private IBoardRoomDao boardroomDao;
 		reserve.setResSign(1);
 		reserve.setResEndtime(resEndtime);
 		JSONObject jobj = new JSONObject();
-		if(reserveDao.save(reserve)) {
+		if (reserveDao.save(reserve)) {
 			jobj.put("mes", "保存成功!");
 			jobj.put("status", "success");
-		}else {
+		} else {
 			jobj.put("mes", "获取失败!");
 			jobj.put("status", "error");
 		}
 		ServletActionContext.getResponse().setHeader("content-type", "text/html;charset=UTF-8");
 		ServletActionContext.getResponse().getWriter().write(jobj.toString());
 		return null;
-		
+
 	}
+
 	/**
 	 * 删除预约信息
+	 * 
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	@Action(value="delete")
-	public String delete() throws IOException{
-		
+	@Action(value = "delete")
+	public String delete() throws IOException {
+
 		String resId = ServletActionContext.getRequest().getParameter("resId");
 		Reserve reserve = reserveDao.getById(resId);
 		JSONObject jobj = new JSONObject();
-		if(reserveDao.delete(reserve)){
-			//save success
+		if (reserveDao.delete(reserve)) {
+			// save success
 			jobj.put("mes", "删除成功!");
 			jobj.put("status", "success");
-		}else{
-			//save failed
+		} else {
+			// save failed
 			jobj.put("mes", "删除失败!");
 			jobj.put("status", "error");
 		}
@@ -130,24 +135,26 @@ private IBoardRoomDao boardroomDao;
 		ServletActionContext.getResponse().getWriter().write(jobj.toString());
 		return null;
 	}
+
 	/**
 	 * 修改预约信息
+	 * 
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	@Action(value="update")
-	public String update() throws IOException{
-		
+	@Action(value = "update")
+	public String update() throws IOException {
+
 		String resId = ServletActionContext.getRequest().getParameter("resId");
 		Reserve reserve = reserveDao.getById(resId);
 		JSONObject jobj = new JSONObject();
-		
-		if(reserveDao.update(reserve)) {
+
+		if (reserveDao.update(reserve)) {
 			jobj.put("mes", "更新成功!");
 			jobj.put("status", "success");
 			jobj.put("loginUser", reserve);
-		}else{
-			//save failed
+		} else {
+			// save failed
 			jobj.put("mes", "更新失败!");
 			jobj.put("status", "error");
 		}
@@ -155,24 +162,25 @@ private IBoardRoomDao boardroomDao;
 		ServletActionContext.getResponse().getWriter().write(jobj.toString());
 		return null;
 	}
-	
+
 	/**
 	 * 根据id信息
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
-	@Action(value="getById")
-	public String getById() throws IOException{
+	@Action(value = "getById")
+	public String getById() throws IOException {
 		String resId = ServletActionContext.getRequest().getParameter("resId");
 		Reserve reserve = reserveDao.getById(resId);
 		JSONObject jobj = new JSONObject();
-		if(reserve != null){
-			//save success
+		if (reserve != null) {
+			// save success
 			jobj.put("mes", "获取成功!");
 			jobj.put("status", "success");
-			jobj.put("data",reserve);
-		}else{
-			//save failed
+			jobj.put("data", reserve);
+		} else {
+			// save failed
 			jobj.put("mes", "获取失败!");
 			jobj.put("status", "error");
 		}
@@ -180,36 +188,38 @@ private IBoardRoomDao boardroomDao;
 		ServletActionContext.getResponse().getWriter().write(jobj.toString());
 		return null;
 	}
+
 	/**
 	 * 获取品牌(类型)列表
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
-	@Action(value="list")
-	public String list() throws IOException{
-		//分页
+	@Action(value = "list")
+	public String list() throws IOException {
+		// 分页
 		String pageNumStr = ServletActionContext.getRequest().getParameter("pageNum");
 		int pageNum = 1;
-		if(pageNumStr!=null && !"".equals(pageNumStr)){
+		if (pageNumStr != null && !"".equals(pageNumStr)) {
 			pageNum = Integer.parseInt(pageNumStr);
 		}
 		List<Object> list = new ArrayList<Object>();
-		List<Object> reserveTypelist = reserveDao.list();//获取所有类型数据，不带分页
-		PageBean page=null;
-		if(reserveTypelist.size()>0){
-			page = new PageBean(reserveTypelist.size(),pageNum,5);
-			list = reserveDao.listAll(page);//带分页
+		List<Object> reserveTypelist = reserveDao.list();// 获取所有类型数据，不带分页
+		PageBean page = null;
+		if (reserveTypelist.size() > 0) {
+			page = new PageBean(reserveTypelist.size(), pageNum, 5);
+			list = reserveDao.listAll(page);// 带分页
 		}
 		JSONObject jobj = new JSONObject();
-		if(reserveTypelist.size() > 0){
-			//save success
+		if (reserveTypelist.size() > 0) {
+			// save success
 			jobj.put("mes", "获取成功!");
 			jobj.put("status", "success");
 			jobj.put("data", JsonUtil.toJsonByListObj(list));
 			jobj.put("pageTotal", page.getPageCount());
 			jobj.put("pageNum", page.getPageNum());
-		}else{
-			//save failed
+		} else {
+			// save failed
 			jobj.put("mes", "获取失败!");
 			jobj.put("status", "error");
 		}
@@ -217,19 +227,71 @@ private IBoardRoomDao boardroomDao;
 		ServletActionContext.getResponse().getWriter().write(jobj.toString());
 		return null;
 	}
-	
-	@Action(value="listAll")
-	public String listAll() throws IOException{
 
-		List<Object> reserveTypelist = reserveDao.list();//获取所有类型数据，不带分页
+	@Action(value = "listAll")
+	public String listAll() throws IOException {
+
+		List<Object> reserveTypelist = reserveDao.list();// 获取所有类型数据，不带分页
 		JSONObject jobj = new JSONObject();
-		if(reserveTypelist.size() > 0){
-			//save success
+		if (reserveTypelist.size() > 0) {
+			// save success
 			jobj.put("mes", "获取成功!");
 			jobj.put("status", "success");
 			jobj.put("data", JsonUtil.toJsonByListObj(reserveTypelist));
-		}else{
-			//save failed
+		} else {
+			// save failed
+			jobj.put("mes", "获取失败!");
+			jobj.put("status", "error");
+		}
+		ServletActionContext.getResponse().setHeader("content-type", "text/html;charset=UTF-8");
+		ServletActionContext.getResponse().getWriter().write(jobj.toString());
+		return null;
+	}
+
+	@Action(value = "searchByCount")
+	public String searchByCount() throws IOException {
+		String resStarttime = ServletActionContext.getRequest().getParameter("resStarttime");
+		String resEndtime = ServletActionContext.getRequest().getParameter("resEndtime");
+		String resBId = ServletActionContext.getRequest().getParameter("resBId");
+
+		Reserve reserve = new Reserve();
+		Reserve reservetwo = new Reserve();
+		Department department = new Department();
+		String dId;
+		int num = 0;
+		String hqlOne = "from Reserve group by resBId where 1=1 and resBId ='" + resBId + "' and resStarttime <= '" + resStarttime
+				+ "' and resStarttime >= '" + resEndtime + "'or resEndtime >= '" + resStarttime
+				+ "' and resEndtime <= '" + resEndtime + "'";
+
+		String hql = "from Reserve dName where 1=1 and resBId ='" + resBId + "' and resStarttime <= '" + resStarttime
+				+ "' and resStarttime >= '" + resEndtime + "'or resEndtime >= '" + resStarttime
+				+ "' and resEndtime <= '" + resEndtime + "'";
+		JSONArray jsonArr = new JSONArray();
+		JSONObject tempJson = new JSONObject();
+		List<Object> reserveTypelist = reserveDao.getAllByConds(hqlOne);//这个就是最后的显示的list吧？这个是去了重的
+		for (int i = 0; i < reserveTypelist.size(); i++) {
+			reserve = (Reserve) reserveTypelist.get(i);
+			dId = reserve.getResDId().getdId();
+			String hqlTwo = hql + "and resDId = '" + dId + "'";
+			//部门的次数
+			num = reserveDao.getAllByConds(hqlTwo).size();//这个list是
+
+			// 部门
+			department = reserve.getResDId();
+			tempJson.put("departId", reserve.getResDId().getdId());
+			tempJson.put("departName", reserve.getResDId().getdName());
+			tempJson.put("useNum", num);
+			jsonArr.add(tempJson);
+		}
+
+		JSONObject jobj = new JSONObject();
+		if (reserveTypelist.size() > 0) {
+			// save success
+			jobj.put("mes", "获取成功!");
+			jobj.put("status", "success");
+			jobj.put("data", jsonArr);
+		} else {
+			// save failed
 			jobj.put("mes", "获取失败!");
 			jobj.put("status", "error");
 		}
@@ -238,5 +300,32 @@ private IBoardRoomDao boardroomDao;
 		return null;
 	}
 	
+	@Action(value = "listAllByBId")
+	public String listAllByBId() throws IOException {
+
+		String resStarttime = ServletActionContext.getRequest().getParameter("resStarttime");
+		String resEndtime = ServletActionContext.getRequest().getParameter("resEndtime");
+		String resBId = ServletActionContext.getRequest().getParameter("resBId");
+		String resDId = ServletActionContext.getRequest().getParameter("resDId");
+		String hql = "from Reserve dName where 1=1  and resDId ='"+resDId+"' and resBId ='" + resBId + "' and resStarttime <= '" + resStarttime
+				+ "' and resStarttime >= '" + resEndtime + "'or resEndtime >= '" + resStarttime
+				+ "' and resEndtime <= '" + resEndtime + "'";
+		System.out.println(hql);
+		List<Object> reserveTypelist = reserveDao.getAllByConds(hql);
+		JSONObject jobj = new JSONObject();
+		if (reserveTypelist.size() > 0) {
+			// save success
+			jobj.put("mes", "获取成功!");
+			jobj.put("status", "success");
+			jobj.put("data", JsonUtil.toJsonByListObj(reserveTypelist));
+		} else {
+			// save failed
+			jobj.put("mes", "获取失败!");
+			jobj.put("status", "error");
+		}
+		ServletActionContext.getResponse().setHeader("content-type", "text/html;charset=UTF-8");
+		ServletActionContext.getResponse().getWriter().write(jobj.toString());
+		return null;
+	}
 
 }
